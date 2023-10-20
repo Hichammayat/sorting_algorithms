0f26cfa0a5cab088f7499851ba182b6b53ce037e
Hichammayat <hichammayat07@gmail.com>
2023-10-20 05:37:37 -0700

    commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int = temp
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i]
+				array[i] = array[i+1]
+				array[i+1] = temp
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		size--;
+	}
+}
