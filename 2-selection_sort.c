83e886a599fdd046f1cf409a13b760fbd93fd60e
Hichammayat <hichammayat07@gmail.com>
2023-10-23 13:58:17 -0700

    commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,20 +1,6 @@
 #include "sort.h"
 
 /**
- * swap_ele - Swap two values in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ele(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
  * selection_sort - Sort an array of integers with selecyion sort algorithm
  * @array: An array of integers.
  * @size: The size of the array.
@@ -23,7 +9,7 @@ void swap_ele(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	int *min;
+	int *min, tmp;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
@@ -37,7 +23,9 @@ void selection_sort(int *array, size_t size)
 
 		if ((array + i) != min)
 		{
-			swap_ele(array + i, min);
+			tmp = *min;
+			*min = array[i];
+			array[i] = tmp;
 			print_array(array, size);
 		}
 	}
